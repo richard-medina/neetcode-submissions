f586f07aeb4f06144e8e0fc9de4134b0da52d34f
richard-medina <r.medina97@proton.me>
2026-06-14 22:22:29 +0200

    Add: is-palindrome - submission-19

---
+++ Data Structures & Algorithms/is-palindrome/submission-19.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        ss = ''.join(x.lower() for x in list(s) if x.isalnum())
+        l = len(ss)
+
+        if l%2 == 0:
+            return all(ss[i] == ss[l-i-1] for i in range((l+1)//2))
+        else:
+            return all(ss[i] == ss[l-i-1] for i in range((l-1)//2))
