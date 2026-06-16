dccc8f8a765b066e054c0d6fad35140c6092ad73
richard-medina <r.medina97@proton.me>
2026-06-16 13:35:33 +0200

    Add: minimum-stack - submission-7

---
+++ Data Structures & Algorithms/minimum-stack/submission-7.py
@@ -0,0 +1,28 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minL = []
+        
+    def push(self, val: int) -> None:
+        if not self.stack:
+            self.minL.append(val)
+        if self.minL and val <= self.minL[-1]:
+            self.minL.append(val)
+        self.stack.append(val)
+        
+
+    def pop(self) -> None:
+        x = self.stack.pop()
+        if x == self.minL[-1]:
+            self.minL.pop()
+        
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        if len(self.stack) == 0:
+            return 0
+        return self.minL[-1]
+        
